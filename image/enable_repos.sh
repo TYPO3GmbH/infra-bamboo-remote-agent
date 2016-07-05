4b87ff87ed8d0b5a88f1701fe75cfad27f786ba0
Christian Kuhn <christian.kuhn@typo3.com>
2016-07-05 15:33:29 +0200

    [TASK] Cleanups to keep branches in sync

--- image/enable_repos.sh
+++ image/enable_repos.sh
@@ -6,13 +6,7 @@ set -x
 ## NGINX Stable Releases
 echo deb http://ppa.launchpad.net/nginx/stable/ubuntu xenial main > /etc/apt/sources.list.d/nginx-stable.list
 
-
 # The recv-keys part takes a bit of time, so it's faster to receive multiple keys at once.
-#
-# Brightbox
-# Phusion Passenger
-# Rowan's Redis PPA
-# OpenJDK 8 PPA
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
 	C300EE8C \
   #
