60f7bb46216fe3f0fb0458751dd8eb5b3df5b2aa
Christian Kuhn <lolli@schwarzbu.ch>
2016-10-19 16:46:27 +0200

    [TASK] Ah, forget wget, too

--- baseimage/utilities.sh
+++ baseimage/utilities.sh
@@ -4,7 +4,7 @@ source /bd_build/buildconfig
 set -x
 
 ## Often used tools.
-$minimal_apt_get_install curl less vim psmisc wget
+$minimal_apt_get_install curl less vim psmisc
 
 ## This tool runs a command as another user and sets $HOME.
 cp /bd_build/bin/setuser /sbin/setuser
