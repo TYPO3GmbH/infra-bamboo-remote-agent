ac1e6fcdb3439acfabf5151400a72531190e40c9
Christian Kuhn <lolli@schwarzbu.ch>
2016-10-19 22:07:12 +0200

    [TASK] Rebase agent 55, 56, 70 images

--- bamboo-remote-agent-php71/config/mysql/grants.sql
+++
@@ -1,2 +0,0 @@
-GRANT ALL ON `func\_%`.* to funcu@'%' IDENTIFIED BY 'funcp' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
--- bamboo-remote-agent-php71/finalize.sh
+++ bamboo-remote-agent-php71/finalize.sh
@@ -3,9 +3,6 @@ set -e
 source /pd_build/buildconfig
 set -x
 
-# remove a ton of packages needed for compilation of apcu and apcu-bc
-apt-get remove -y --purge autoconf automake autotools-dev binutils cpp cpp-5 file gcc gcc-5 libasan2 libatomic1 libc-dev-bin libc6-dev libcc1-0 libcilkrts5 libgcc-5-dev libisl15 libitm1 liblsan0 libmpc3 libmpfr4 libmpx0 libpcre16-3 libpcre3-dev libpcre32-3 libpcrecpp0v5 libquadmath0 libsigsegv2 libssl-dev libtool libtsan0 libubsan0 linux-libc-dev m4 make php7.1-dev shtool zlib1g-dev
-
 apt-get clean
 rm -rf \
 	/var/lib/apt/lists/* \
