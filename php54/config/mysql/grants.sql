6d5e3f4d8488cdc274c48f4e5be92e4d9e4cbd1c
Christian Kuhn <lolli@schwarzbu.ch>
2016-10-24 20:20:17 +0200

    [TASK] Kickstart php54

---
+++ php54/config/mysql/grants.sql
@@ -0,0 +1,2 @@
+GRANT ALL ON `func\_%`.* to funcu@'%' IDENTIFIED BY 'funcp' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
---
+++ php54/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+source /pd_build/buildconfig
+set -x
+
+/pd_build/prepare.sh
+/pd_build/mysql.sh
+/pd_build/redis-server.sh
+/pd_build/memcached.sh
+/pd_build/php.sh
+
+/pd_build/finalize.sh
