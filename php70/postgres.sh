4450da137a24f2e0207cf5b4ea48e2bbd77c55c8
Christian Kuhn <lolli@schwarzbu.ch>
2017-02-16 00:42:44 +0100

    [TASK] Add root as super user to postgres

--- php70/postgres.sh
+++ php70/postgres.sh
@@ -7,12 +7,3 @@ set -x
 minimal_apt_get_install \
   postgresql \
   #
-
-## Additional config files
-#cp /pd_build/config/mysql/*.cnf /etc/mysql/mariadb.conf.d/
-
-## Provide grants.sql
-#cp /pd_build/config/mysql/grants.sql /etc/mysql/grants.sql
-
-## Enable mysql
-#cp -a /pd_build/runit/mysql /etc/service/mysql
