e916ba198392fa64263e0e51ea5a5f8c7b4493e7
Christian Kuhn <lolli@schwarzbu.ch>
2017-02-15 21:30:39 +0100

    [TASK] postgres kickstart, disable cleanup

--- php70/finalize.sh
+++ php70/finalize.sh
@@ -46,12 +46,12 @@ apt-get remove -y --purge \
   #
 
 apt-get clean
-rm -rf \
-	/var/lib/apt/lists/* \
-	/tmp/* \
-	/var/tmp/* \
-	/usr/local/src/* \
-	/usr/include/php/20151012/ext/apcu/ \
+#rm -rf \
+#	/var/lib/apt/lists/* \
+#	/tmp/* \
+#	/var/tmp/* \
+#	/usr/local/src/* \
+#	/usr/include/php/20151012/ext/apcu/ \
 	#
 
 rm -rf /pd_build
