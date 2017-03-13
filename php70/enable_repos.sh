c733e3b25b3266ce12b00470cccbe80ac7abf94f
Christian Kuhn <lolli@schwarzbu.ch>
2017-03-13 19:59:48 +0100

    [TASK] Swap repo list to suppress dupe apt-get update call

--- php70/enable_repos.sh
+++ php70/enable_repos.sh
@@ -3,10 +3,10 @@ set -e
 source /pd_build/buildconfig
 set -x
 
-## NodeSource's Node.js repository
-curl --fail -sL https://deb.nodesource.com/setup_4.x | bash -
-
 # mssql and tools
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/mssql-server.list | tee /etc/apt/sources.list.d/mssql-server.list
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | tee /etc/apt/sources.list.d/msprod.list
+
+## NodeSource's Node.js repository
+curl --fail -sL https://deb.nodesource.com/setup_4.x | bash -
