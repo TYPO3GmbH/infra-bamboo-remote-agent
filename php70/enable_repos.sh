54f35e58c29f5ca2dc82a2b3507424226ba152af
Christian Kuhn <lolli@schwarzbu.ch>
2017-04-23 18:08:14 +0200

    [TASK] Next try running ac tests with chrome

--- php70/enable_repos.sh
+++ php70/enable_repos.sh
@@ -8,5 +8,9 @@ curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/mssql-server.list | tee /etc/apt/sources.list.d/mssql-server.list
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | tee /etc/apt/sources.list.d/msprod.list
 
-## NodeSource's Node.js repository
+# google-chrome
+curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+
+# NodeSource's Node.js repository
 curl --fail -sL https://deb.nodesource.com/setup_4.x | bash -
--- php70/install.sh
+++ php70/install.sh
@@ -12,6 +12,7 @@ set -x
 /pd_build/memcached.sh
 /pd_build/nodejs.sh
 /pd_build/php.sh
+/pd_build/chrome.sh
 /pd_build/firefox.sh
 
 /pd_build/finalize.sh
