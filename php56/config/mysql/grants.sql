43c94733f173537a61ef5ad7552afc2e1d850187
Christian Kuhn <lolli@schwarzbu.ch>
2018-05-05 23:20:40 +0200

    [TASK] Extend mariadb grants - younger version seems to be more picky

--- php56/config/mysql/grants.sql
+++ php56/config/mysql/grants.sql
@@ -1,2 +1,3 @@
 GRANT ALL ON `func\_%`.* to funcu@'%' IDENTIFIED BY 'funcp' WITH GRANT OPTION;
+GRANT ALL ON `func\_%`.* to funcu@'localhost' IDENTIFIED BY 'funcp' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
