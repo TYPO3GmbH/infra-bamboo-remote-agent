4cde96726590433c9b8228184356e96918ab93ed
Christian Kuhn <lolli@schwarzbu.ch>
2018-10-19 16:30:47 +0200

    [TASK] infra-352 Relax access rights on environment scripts for now

--- bamboo-remote-agent/system_services.sh
+++ bamboo-remote-agent/system_services.sh
@@ -10,11 +10,9 @@ mkdir -p /etc/my_init.d
 mkdir -p /etc/container_environment
 touch /etc/container_environment.sh
 touch /etc/container_environment.json
-chmod 700 /etc/container_environment
+chmod 777 /etc/container_environment
 
-groupadd -g 8377 docker_env
-chown :docker_env /etc/container_environment.sh /etc/container_environment.json
-chmod 640 /etc/container_environment.sh /etc/container_environment.json
+chmod 777 /etc/container_environment.sh /etc/container_environment.json
 ln -s /etc/container_environment.sh /etc/profile.d/
 
 # Install runit.
