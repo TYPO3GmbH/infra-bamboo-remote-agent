7c92480d555c619c1bc09facf26bc9e5b2b84396
Christian Kuhn <lolli@schwarzbu.ch>
2018-10-19 16:14:43 +0200

    [TASK] infra-352 bamboo agent environment handling

--- bamboo-remote-agent/system_services.sh
+++ bamboo-remote-agent/system_services.sh
@@ -7,5 +7,15 @@ set -x
 cp /pd_build/bin/my_init /sbin/
 mkdir -p /etc/my_init.d
 
+mkdir -p /etc/container_environment
+touch /etc/container_environment.sh
+touch /etc/container_environment.json
+chmod 700 /etc/container_environment
+
+groupadd -g 8377 docker_env
+chown :docker_env /etc/container_environment.sh /etc/container_environment.json
+chmod 640 /etc/container_environment.sh /etc/container_environment.json
+ln -s /etc/container_environment.sh /etc/profile.d/
+
 # Install runit.
 minimal_apt_get_install runit
