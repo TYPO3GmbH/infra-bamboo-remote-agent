c2cdcd3d4039841f2797ae61e95fe16c2560a000
Christian Kuhn <lolli@schwarzbu.ch>
2018-12-15 12:31:04 +0100

    [TASK] Update php download mirror for old php versions

--- php53/php.sh
+++ php53/php.sh
@@ -149,7 +149,7 @@ ln -s x86_64-linux-gnu/curl
 
 cd /usr/local/src
 
-curl -SL --progress-bar http://in1.php.net/distributions/php-5.3.29.tar.bz2 -o php-5.3.29.tar.bz2
+curl -SL --progress-bar http://de2.php.net/distributions/php-5.3.29.tar.bz2 -o php-5.3.29.tar.bz2
 tar -xvf php-5.3.29.tar.bz2
 cd php-5.3.29
 
