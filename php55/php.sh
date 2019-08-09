50c5667759e4749466d179c995a986b181eb2cf2
Christian Kuhn <lolli@schwarzbu.ch>
2019-08-09 12:14:48 +0200

    [BUGFIX] php 5.5 and 5.6 now need explicit phpredis 4.2.0 since younger phpredis dropped php 5.x support

--- php55/php.sh
+++ php55/php.sh
@@ -205,10 +205,11 @@ sed -i s/';date.timezone ='/'date.timezone = UTC'/ /usr/local/lib/php.ini
 # hint for default socket
 sed -i s%'mysqli.default_socket ='%'mysqli.default_socket = /var/run/mysqld/mysqld.sock'% /usr/local/lib/php.ini
 
-# phpredis
+# phpredis - last version that supports php 5.x is 4.2.0
 cd /usr/local/src
 git clone https://github.com/phpredis/phpredis.git
 cd phpredis
+git checkout 4.2.0
 phpize
 ./configure
 make && make install
